e66a1a0ffc3f988a4fb460aa9a4df29c21b7e7c6
fenexia/svirisov <stanislav.irisov@student.manchester.ac.uk>
2025-03-04 11:17:21 +0000

    Added debug file to test core functionality
    - setup and toggle single pin (8) high/low

---
+++ debug.py
@@ -0,0 +1,15 @@
+import time
+import RPi.GPIO as io
+
+io.setmode(io.BOARD)
+
+io.setup(8, io.IN, initial=0)
+
+print('finished setup')
+
+for i in range(0,5):
+    io.output(8,1)
+    print('high')
+    time.sleep(5)
+    io.output(8,0)
+    print('low')
