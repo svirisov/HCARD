e7f94282792a56e945c9e52d6b84fb7a79042cf9
fenexia/svirisov <stanislav.irisov@student.manchester.ac.uk>
2025-03-09 16:30:36 +0000

    De/Mux Test

--- debug.py
+++ debug.py
@@ -3,14 +3,42 @@ import RPi.GPIO as io
 
 io.setmode(io.BOARD)
 
+pinDict = { 'rowSelect1' : 3,
+            'rowSelect2' : 5,
+            'rowSelect3' : 7,
+            'voltageRead' : 40,
+            'muxEnable': 16
+        }
+
+# General setup
+io.setmode(io.BOARD)
+io.setwarnings(False)
+
+# Pin configurations
+io.setup(pinDict['rowSelect1'], io.OUT, initial = io.LOW) 
+io.setup(pinDict['rowSelect2'], io.OUT, initial = io.LOW) 
+io.setup(pinDict['rowSelect3'], io.OUT, initial = io.LOW) 
+io.setup(pinDict['voltageRead'], io.IN, pull_up_down = io.PUD_DOWN) # Default LOW (0)
+io.setup(pinDict['muxEnable'], io.OUT, initial = io.LOW) 
+
 io.setup(8, io.OUT, initial=io.LOW)
 
 print('finished setup')
 
-for i in range(0,5):
-    io.output(8,1)
-    print('high')
+readSequence = [(0,0,0),
+                (0,0,1),
+                (0,1,0),
+                (0,1,1),
+                (1,0,0),
+                (1,0,1),
+                (1,1,1)]
+
+for i, vals  in enumerate(readSequence):
+    io.output(pinDict['rowSelect1'],vals[0])
+    io.output(pinDict['rowSelect2'],vals[1])
+    io.output(pinDict['rowSelect3'],vals[2])
+    print('vals set')
     time.sleep(5)
-    io.output(8,0)
+    input = io.input(pinDict['voltageRead'])
     print('low')
     time.sleep(5)
