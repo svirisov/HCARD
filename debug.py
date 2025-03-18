9c716bcad36eea8274b24a50af673fd1f8bb7546
fenexia/svirisov <stanislav.irisov@student.manchester.ac.uk>
2025-03-18 11:42:54 +0000

    Only test motors

--- debug.py
+++ debug.py
@@ -53,19 +53,20 @@ motorSequence = [(0,0,0),
 
 try:
     while True:
-        for i, vals in enumerate(readSequence):
-            io.output(pinDict['rowSelect1'],vals[0])
-            io.output(pinDict['rowSelect2'],vals[1])
-            io.output(pinDict['rowSelect3'],vals[2])
-            #print('vals set')
-            time.sleep(.1)
-            input = io.input(pinDict['voltageRead'])
-            print(f'Read: {input}')
-            time.sleep(.2)
+#        for i, vals in enumerate(readSequence):
+#            io.output(pinDict['rowSelect1'],vals[0])
+#            io.output(pinDict['rowSelect2'],vals[1])
+#            io.output(pinDict['rowSelect3'],vals[2])
+#            #print('vals set')
+#            time.sleep(.1)
+#            input = io.input(pinDict['voltageRead'])
+#            print(f'Read: {input}')
+#            time.sleep(.2)
         for i, vals in enumerate(motorSequence):
             io.output(pinDict['motorSelect1'],vals[0])
             io.output(pinDict['motorSelect2'],vals[1])
             io.output(pinDict['motorSelect3'],vals[2])
             time.sleep(5)
+            print(f'Vals set {vals[0]}{vals[2]}{vals[2]}')
 except KeyboardInterrupt:
     print('Process ended')
